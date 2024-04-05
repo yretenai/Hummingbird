426cc64d7c61517ba3e700cc936e5472f30ac905
Ada <ada@chronovore.dev>
2024-04-05 01:32:51 +0100

    Swift init

---
+++ Sources/Larvivora/main.swift
@@ -0,0 +1,6 @@
+// larvivora project
+// Copyright (c) 2024 <https://github.com/yretenai/Larvivora>, <https://hg.sr.ht/~chronovore/larvivora/browse>
+// SPDX-License-Identifier: MPL-2.0
+
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
---
+++ Tests/LarvivoraTests/LarvivoraTests.swift
@@ -0,0 +1,16 @@
+// larvivora project
+// Copyright (c) 2024 <https://github.com/yretenai/Larvivora>, <https://hg.sr.ht/~chronovore/larvivora/browse>
+// SPDX-License-Identifier: MPL-2.0
+
+import XCTest
+@testable import Larvivora
+
+final class LarvivoraTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
