ada1c3f5dc1265a4bbcd5b68355bbc18aed7c69f
Ada <ada@chronovore.dev>
2024-04-05 02:05:42 +0100

    use tabs

--- Package.swift
+++ Package.swift
@@ -1,32 +1,29 @@
+// swift-tools-version: 5.10
+
 // larvivora project
 // Copyright (c) 2024 <https://github.com/yretenai/Larvivora>, <https://hg.sr.ht/~chronovore/larvivora/browse>
 // SPDX-License-Identifier: MPL-2.0
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Larvivora",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .executable(
-            name: "Larvivora",
-            targets: ["Larvivora"]),
-        .library(
-            name: "LarvivoraKit",
-            targets: ["LarvivoraKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Larvivora",
-            dependencies: ["LarvivoraKit"]),
-        .target(
-            name: "LarvivoraKit"),
-        .testTarget(
-            name: "LarvivoraKitTests",
-            dependencies: ["LarvivoraKit"]),
-    ]
+	name: "Larvivora",
+	products: [
+		.executable(
+			name: "Larvivora",
+			targets: ["Larvivora"]),
+		.library(
+			name: "LarvivoraKit",
+			targets: ["LarvivoraKit"]),
+	],
+	targets: [
+		.executableTarget(
+			name: "Larvivora",
+			dependencies: ["LarvivoraKit"]),
+		.target(
+			name: "LarvivoraKit"),
+		.testTarget(
+			name: "LarvivoraKitTests",
+			dependencies: ["LarvivoraKit"]),
+	]
 )
--- Tests/LarvivoraTests/LarvivoraTests.swift
+++ Tests/LarvivoraTests/LarvivoraTests.swift
@@ -6,11 +6,11 @@ import XCTest
 @testable import Larvivora
 
 final class LarvivoraTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+	func testExample() throws {
+		// XCTest Documentation
+		// https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+		// Defining Test Cases and Test Methods
+		// https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+	}
 }
